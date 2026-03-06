af1fb54ccdaaf5ef5fb68e8a0957199219c39f4e
GunzaPapper <gunzapepper@gmail.com>
2026-03-07 01:26:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ function esc(s) {
 }
 
 function disablePinchZoom() {
-  document.("gesturestart", (e) => e.preventDefault(), { passive: false });
+  document.addEventListener("gesturestart", (e) => e.preventDefault(), { passive: false });
   document.addEventListener("gesturechange", (e) => e.preventDefault(), { passive: false });
   document.addEventListener("gestureend", (e) => e.preventDefault(), { passive: false });
 
